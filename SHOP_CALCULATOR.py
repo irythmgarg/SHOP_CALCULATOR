113beab03677667907126f8959f80654c5d8263d
irythmgarg <156564516+irythmgarg@users.noreply.github.com>
2024-01-27 12:33:45 +0530

    hello

--- SHOP_CALCULATOR.py
+++ SHOP_CALCULATOR.py
@@ -1,7 +1,8 @@
+#calculator to add bill
 sum =0
 while (True):
   number=(input("enter the item price or press q to exit : \n"))
-  if(number!="q" or number!="q"):
+  if(number!="q" or number!="q"):# if all items done
    sum+=int(number)
   else :
    print(f"\nYOUR TOTAL AMOUNT IS {sum}\n")
