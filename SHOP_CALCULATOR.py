a52ab8b5bf9df596bc6ffc3d4091f8b795578e2f
irythmgarg <156564516+irythmgarg@users.noreply.github.com>
2024-01-27 12:32:06 +0530

    hello

---
+++ SHOP_CALCULATOR.py
@@ -0,0 +1,9 @@
+sum =0
+while (True):
+  number=(input("enter the item price or press q to exit : \n"))
+  if(number!="q" or number!="q"):
+   sum+=int(number)
+  else :
+   print(f"\nYOUR TOTAL AMOUNT IS {sum}\n")
+   print("THANK YOU FOR YOUR KIND VISIT\n")
+   break
